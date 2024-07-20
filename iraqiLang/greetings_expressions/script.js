7d8efb9bf107880a93ceb1ae5ce0d127a28b16e4
momaki9 <mostafa_m9@yahoo.com>
2024-07-20 07:49:49 -0700

    added iraqi material

---
+++ iraqiLang/greetings_expressions/script.js
@@ -0,0 +1,58 @@
+// variables and constants
+const wordElm = document.getElementById("word");
+const defElm = document.getElementById("def");
+const prevBtn = document.getElementById("btn0");
+const meaningBtn = document.getElementById("btn1");
+const nextBtn = document.getElementById("btn2");
+
+const shuffledArr = shuffleArray(words);
+
+let selectedElm = shuffledArr[0];
+
+wordElm.textContent = selectedElm.word;
+
+let value = Number(nextBtn.getAttribute("data"));
+
+if ((value > 0 ) && (value <= words.length-1)) {
+    prevBtn.disabled = false;
+} else if (value === 0) {
+    prevBtn.disabled = true;
+};
+
+nextBtn.addEventListener("click", () => {
+    if (value >= 0) {
+        prevBtn.disabled = false;
+    }
+    defElm.textContent = "";
+    if (value === words.length - 1) {
+        nextBtn.disabled = true;
+    } else {
+        value = value += 1;
+        if (value === words.length -1) {
+            nextBtn.disabled = true;
+        }
+    }
+    nextBtn.setAttribute("data", value);
+    wordElm.textContent = shuffledArr[value].word;
+});
+
+prevBtn.addEventListener("click", () => {
+    defElm.textContent = "";
+    if (value <= 1) {
+        prevBtn.disabled = true;
+    }
+    value--;
+    if (value <= words.length - 2) {
+        nextBtn.disabled = false;
+    }
+    nextBtn.setAttribute("data", value);
+    wordElm.textContent = shuffledArr[value].word;
+});
+
+meaningBtn.addEventListener("click", () => {
+    defElm.textContent = shuffledArr[value].def;
+});
+
+document.getElementById("playBtn").addEventListener("click", ()=>{
+    responsiveVoice.speak(shuffledArr[value].word, 'Arabic Female');
+});
---
+++ iraqiLang/greetings_expressions/words.js
@@ -0,0 +1,74 @@
+const words = [
+    {
+        word: "مَرْحَبة",
+        def: "Hello"
+    },
+    {
+        word: "شْلونَك؟",
+        def: "How are you? (speaking to a male)"
+    },
+    {
+        word: "شْلونِـچ؟",
+        def: "How are you? (speaking to a female)"
+    },
+    {
+        word: "آني زَين. الحَمْدُ لله.",
+        def: "I am well. Praise God. (Male speaking)"
+    },
+    {
+        word: "آني زَينه. الحَمْدُ لله.",
+        def: "I am well. Praise God. (Feale speaking)"
+    },
+    {
+        word: "شْلون الأهِل؟",
+        def: "How is your family doing?"
+    },
+    {
+        word: "بْخير نُشْكُر الله",
+        def: "(They are) Well thanks to God"
+    },
+    {
+        word: "تْشَرَّفْنا",
+        def: "Pleased (or honored) to meet you"
+    },
+    {
+        word: "وِلنا الشَّرَف",
+        def: "The pleasure (or honor) is all mine (response)"
+    },
+    {
+        word: "تْفَضَّل إسْتَريح",
+        def: "Please have a seat"
+    },
+    {
+        word: "عَن إذنَك",
+        def: "Excuse me"
+    },
+    {
+        word: "جوعان",
+        def: "Hungry"
+    },
+    {
+        word: "تَعبان",
+        def: "Tired"
+    },
+    {
+        word: "فَرحان",
+        def: "Happy"
+    },
+    {
+        word: "عَطشان",
+        def: "Thirsty"
+    },
+    {
+        word: "شَكو ماكو اليوم؟",
+        def: "What's new today?"
+    },
+    {
+        word: "ماكو شي اليوم",
+        def: "Nothing is new today"
+    },
+    {
+        word: "الله بِالخير",
+        def: "God bless (greeting)"
+    }
+]       
